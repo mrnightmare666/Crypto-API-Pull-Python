269aa353ae83035211c31582f67823dbc2180280
Nikhil <53763049+mrnightmare666@users.noreply.github.com>
2023-06-14 23:33:01 +0530

    Update code.py
    Better comments and limited redunted code.

--- code.py
+++ code.py
@@ -1,37 +1,43 @@
+import time
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 #provides functions for working with JSON data in Python
 import json
+import pandas as pd
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert':'USD'
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'f5f31361-b943-4caf-b7f9-f4d767f04f10',
-}
+df = pd.DataFrame()
 
-session = Session()
-session.headers.update(headers)
+def api_pull():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    parameters = {
+        'start': '1',
+        'limit': '25',
+        'convert': 'USD'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': 'f5f31361-b943-4caf-b7f9-f4d767f04f10',
+    }
 
-try:
-  #sends an HTTP GET request to the specified url with the given parameters
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  #Making the data more presentable
-  prettified_response = json.dumps(data, indent=4)
-  print(prettified_response)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
-  
-  import pandas as pd
-  
-#This allows you to see all the columns, not just like 15
-pd.set_option('display.max_columns', None)
+    session = Session()
+    session.headers.update(headers)
 
-#Presents the data in a table format
-pd.json_normalize(data['data'])
+    try:
+        #sends an HTTP GET request to the specified url with the given parameters
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        #Making the data more presentable
+        prettified_response = json.dumps(data, indent=4)
+
+        global df  # Use the global DataFrame variable
+        df = pd.concat([df, pd.json_normalize(data['data'])], ignore_index=True)
+
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
+# Run the API call and DataFrame update every 10 minutes for 1 hour
+for i in range(6):
+    api_pull()
+    print("API Call completed")
+    time.sleep(600)  # Delay for 10 minutes
